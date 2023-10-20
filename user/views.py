4a3b8128096c7a6f178f8d32226dc0a0e0485524
Munkoor <eee1lvlboss@gmail.com>
2023-10-20 16:09:50 +0300

    Wrote CreateUserView  and  new endpoint 'register'

--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
+from rest_framework import generics
 from django.shortcuts import render
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
