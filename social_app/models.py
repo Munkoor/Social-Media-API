08321a053cd9ea189ab4bf4cb78a8ab07b5ba8c0
Munkoor <eee1lvlboss@gmail.com>
2023-10-21 20:23:37 +0300

    Modified my models in social app

--- social_app/models.py
+++ social_app/models.py
@@ -15,19 +15,42 @@ def image_file_path(instance, filename):
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    class GenderChoices(models.TextChoices):
+        MALE = "Male"
+        FEMALE = "Female"
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField(blank=True)
+    gender = models.CharField(
+        max_length=63, choices=GenderChoices.choices, null=True
+    )
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(
         upload_to=image_file_path, blank=True
     )
+    followed = models.ManyToManyField(
+        "self", symmetrical=False, blank=True, related_name="followers"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              related_name="user_comments")
     content = models.TextField()
     image = models.ImageField(upload_to=image_file_path,
                               blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
 
 
 class Like(models.Model):
@@ -37,16 +60,11 @@ class Like(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="post_comments")
     text = models.TextField()
+    created_at = models.TimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["created_at"]
 
-class Follow(models.Model):
-    follower = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name="following"
-    )
-    following = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name="followers"
-    )
