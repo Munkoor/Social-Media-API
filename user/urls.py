fff3df11bd185dd8e105ac696a284612ba3f9f79
Munkoor <eee1lvlboss@gmail.com>
2023-10-22 11:41:28 +0300

    Added and realized new endpoint for logout in user app

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,7 +13,8 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("me/", ManageUserView.as_view(), name="profile")
+    path("me/", ManageUserView.as_view(), name="profile"),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 app_name = "user"
